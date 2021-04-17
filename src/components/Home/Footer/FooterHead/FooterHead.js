5259b246daa7bffb8e4c48b97ccf6526fb543472
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-04-17 22:56:48 +0600

    Complete-website

--- src/components/Home/Footer/FooterHead/FooterHead.js
+++ src/components/Home/Footer/FooterHead/FooterHead.js
@@ -8,7 +8,7 @@ const FooterHead = () => {
         <div className="container ">
            <div className="row footer-header pt-2 d-flex align-items-center">
                <div className="col-md-6">
-               <h2 className="nav-brand">BD <span style={{color:'#42B6E1'}}>net</span></h2>
+               <h2 className="nav-brand">Jelly <span style={{color:'#42B6E1'}}>net</span></h2>
                </div>
                <div className="col-md-6">
                    <h6> <FontAwesomeIcon icon={faYoutube} /> Find tutorials and demos</h6>
